71e869613c5b0e475be2318ffb8c9e815d092ae8
fadeyibi gbolahan <fadeyibi26@gmail.com>
2025-04-02 15:24:27 +0100

    dist to build

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: "/", // Replace with your actual GitHub repo name
     server: {
         port: 3000,
     },
