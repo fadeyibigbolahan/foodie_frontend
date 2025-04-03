2cf746422467d780a0a79a9b616e0963d80a39ef
fadeyibi gbolahan <fadeyibi26@gmail.com>
2025-04-03 11:18:37 +0100

    changes

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "/foodie_frontend/", // Replace with your actual GitHub repo name
+    base: "/", // Replace with your actual GitHub repo name
     server: {
         port: 3000,
     },
