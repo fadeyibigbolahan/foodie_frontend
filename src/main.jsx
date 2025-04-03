a91668cbba034332f623d97826edd9ddf7cf2cc1
fadeyibi gbolahan <fadeyibi26@gmail.com>
2025-04-03 12:11:35 +0100

    deep

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { AuthProvider } from "./contexts/AuthContext.jsx";
+import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <AuthProvider>
-            <App />
-        </AuthProvider>
+        <HashRouter basename="/foodie_frontend">
+            <AuthProvider>
+                <App />
+            </AuthProvider>
+        </HashRouter>
     </StrictMode>,
 );
